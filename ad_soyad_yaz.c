de70859e590b31e60904aee098a3d8da317ee842
sehribanmutlu <119937838+sehribanmutlu@users.noreply.github.com>
2022-12-13 10:10:37 +0300

    sehriban mutlu
    @themanoftalent

--- ad_soyad_yaz.c
+++ ad_soyad_yaz.c
@@ -7,11 +7,20 @@
 #include <stdio.h>
 
 int main() {
+char name[30],surname[20];
+  int age;
+  printf("enter your name");
+  scanf("%s",name);
+  printf("enter your surname");
+  scanf("%s",surname);
+   printf("enter your age");
+  scanf("%d",&age);
+  
+  printf("your name %s your surname %s your age %d",name,surname,age);
+  return 0:
+  
 
-
-  //CODE BUARAYA YAZILACAK 
-//   String icin char kullan ve &kullanma, & sadece sayilar icin kullan. 
-
+  
 
 
 
