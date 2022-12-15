44784b9294ce7692d933b47a772c60d30ca81d89
sehribanmutlu <119937838+sehribanmutlu@users.noreply.github.com>
2022-12-15 17:48:12 +0300

    @sehribanmutlu
    @themanoftalent

--- Odevler/TenNumbers.c
+++ Odevler/TenNumbers.c
@@ -1,9 +1,21 @@
 //AKIF
 //C program to input 10 numbers from user and display 10 numbers using array.
 
-#include <stdio.h>
-int main()
-{
-//   YOUR CODE HERE
-  
+include <stdio.h>
+
+int main(){
+	int intArray[10];
+	int sum=0,i,j;
+	
+	for(i=0;i<10;i++){
+		printf("enter the number:");
+		scanf("%d",&intArray[i]);
+	}
+	for(j=0;j<10;j++){
+		printf("%d",intArray[j]);
+	}
+	
+
+
+	
 }
